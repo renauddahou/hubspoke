e495824205f6dccf2012a61d436ce0047369be9b
renaud <root@DESKTOP-FTU31JP>
2025-04-13 23:17:19 +0200

    up trigger

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     aws = {
+      region = var.region
       source  = "hashicorp/aws"
       version = ">= 1.11.3"
     }
   }
 }
 
-# Provider pour AWS
-provider "aws" {
-  region = var.region
-}
